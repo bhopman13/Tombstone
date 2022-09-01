93d06f87b195ab4a6a8a59ab7e5c0e0cfd8dd042
Jamie <bhopman13@gmail.com>
2022-09-01 16:57:46 +0100

    Changing to pure tombstone

--- src/main/java/com/jl/main/Tombstone.java
+++ src/main/java/com/jl/main/Tombstone.java
@@ -24,67 +24,19 @@ import java.util.logging.Level;
 public class Tombstone extends JavaPlugin {
 
     private Storage storage;
-    private boolean skills = false;
-    private AureliumSkills skills_plugin;
-    private BukkitTask task;
-    HashMap<String, Double> multipliers = new HashMap<>();
-    HashMap<String, BukkitTask> tasks = new HashMap<>();
     @Override
     public void onEnable() {
         getDataFolder().mkdir();
         storage = new Storage(this);
         CustomMap<Location, Inventory> inventories = storage.loadMap();
 
-        getServer().getPluginManager().registerEvents(new DeathListener(inventories, this, multipliers), this);
+        getServer().getPluginManager().registerEvents(new DeathListener(inventories, this), this);
         getServer().getPluginManager().registerEvents(new ChestOpenListener(inventories, this), this);
         getServer().getPluginManager().registerEvents(new ChestCloseListener(inventories, this), this);
         getServer().getPluginManager().registerEvents(new ChestBreakListener(inventories, this), this);
 
-        if(getServer().getPluginManager().getPlugin("AureliumSkills") != null){
-            skills = true;
-            skills_plugin = AureliumAPI.getPlugin();
 
-            try {
-                loadMults();
-            } catch (IOException e) {
-                skills = false;
-                return;
-            }
-            getServer().getPluginManager().registerEvents(new PlayerJoinListener(this, multipliers, tasks), this);
-            getServer().getPluginManager().registerEvents(new PlayerLeaveListener(this, tasks), this);
-            saveMultsTask();
 
-        }
-
-    }
-    FileConfiguration customConfig;
-    File customYml;
-    private void loadMults() throws IOException {
-
-        try {
-            customYml = new File(getDataFolder() + "/multipliers.dat");
-            customConfig = YamlConfiguration.loadConfiguration(customYml);
-            multipliers = (HashMap<String, Double>) customConfig.get("multipliers");
-        }catch(Exception e){
-            customYml = new File(getDataFolder() + "/multipliers.dat");
-            customYml.delete();
-            customYml.createNewFile();
-            customConfig = YamlConfiguration.loadConfiguration(customYml);
-        }
-    }
-    private void saveMultsTask(){
-        BukkitScheduler scheduler = getServer().getScheduler();
-        task = scheduler.runTaskTimerAsynchronously(this, () -> {
-            saveMults();
-        }, 12000L, 12000L);
-    }
-    private void saveMults(){
-        customConfig.set("multipliers", multipliers);
-        try {
-            customConfig.save(customYml);
-        } catch (IOException e) {
-            getLogger().log(Level.SEVERE, "Error saving multipliers");
-        }
     }
 
     public void saveMap() {
@@ -94,17 +46,9 @@ public class Tombstone extends JavaPlugin {
     @Override
     public void onDisable() {
         saveMap();
-        if(task != null) {task.cancel();}
-        saveMults();
         getLogger().info("Tombstone chests saved. Closing down.");
     }
 
-    public boolean isAureliumLoaded(){
-        return skills;
-    }
 
-    public AureliumSkills getAureliumPlugin(){
-        return skills_plugin;
-    }
 
 }
